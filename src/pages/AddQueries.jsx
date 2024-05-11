41bd208c398947e5da7cecb9b0eb2d3c140d0ffb
jakareya76 <jakareya1306@gmail.com>
2024-05-11 13:22:48 +0600

    create add queries and api base url with axios

--- src/pages/AddQueries.jsx
+++ src/pages/AddQueries.jsx
@@ -1,5 +1,87 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
+import api from "../api/api";
+
 const AddQueries = () => {
-  return <div>AddQueries</div>;
+  const { user } = useContext(AuthContext);
+
+  const handleAddQueries = async (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const productName = form.productName.value;
+    const productBrand = form.productBrand.value;
+    const photoURL = form.photoURL.value;
+    const queryTitle = form.queryTitle.value;
+    const boycottingReason = form.boycottingReason.value;
+
+    const userName = user?.displayName;
+    const userEmail = user?.email;
+
+    const queries = {
+      productName,
+      productBrand,
+      photoURL,
+      queryTitle,
+      boycottingReason,
+      userEmail,
+      userName,
+      recommendationCount: 0,
+    };
+
+    try {
+      const res = await api.post(`/add-queries`, queries);
+
+      if (res.data.acknowledged) {
+        form.reset();
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div className="container px-5 mx-auto">
+      <div className="max-w-4xl mx-auto my-10">
+        <h2 className="my-8 text-2xl font-bold text-center">Add A Queries</h2>
+        <form onSubmit={handleAddQueries}>
+          <div className="grid grid-cols-1 gap-5 my-5 md:grid-cols-2">
+            <input
+              type="text"
+              name="productName"
+              placeholder="Product Name"
+              className="px-4 input input-bordered"
+            />
+            <input
+              type="text"
+              name="productBrand"
+              placeholder="Product Brand"
+              className="px-4 input input-bordered"
+            />
+            <input
+              type="text"
+              name="photoURL"
+              placeholder="Product Image-URL"
+              className="px-4 input input-bordered"
+            />
+            <input
+              type="text"
+              name="queryTitle"
+              placeholder="Query TItle"
+              className="px-4 input input-bordered"
+            />
+            <textarea
+              name="boycottingReason"
+              placeholder="Boycotting Reason Details"
+              className="h-24 px-5 md:col-span-2 textarea textarea-bordered"
+            ></textarea>
+          </div>
+          <button className="w-full btn btn-primary">Add Query</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddQueries;
--- src/pages/MyQueries.jsx
+++ src/pages/MyQueries.jsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom";
+
 const MyQueries = () => {
-  return <div>MyQueries</div>;
+  return (
+    <div>
+      <div
+        style={{
+          backgroundImage: `url(https://images.pexels.com/photos/919734/pexels-photo-919734.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`,
+        }}
+        className="flex flex-col items-center justify-center gap-4 py-16 bg-no-repeat bg-cover"
+      >
+        <h2 className="text-2xl font-bold text-white">Explore Your Queries</h2>
+        <Link to="/add-queries" className="px-8 btn btn-primary">
+          Add Query
+        </Link>
+      </div>
+
+      <div className="my-10">
+        <h2 className="text-3xl font-bold text-center">My Query</h2>
+
+        <p className="my-10 text-center text-red-500">
+          No Query Found Please Add One!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default MyQueries;
