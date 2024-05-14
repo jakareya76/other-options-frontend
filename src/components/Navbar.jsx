b362ca9b68bf52f6324fc097f496593e913dbe2b
jakareya76 <jakareya1306@gmail.com>
2024-05-14 11:03:02 +0600

    add toggle theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -103,6 +103,45 @@ const Navbar = () => {
               </li>
             </>
           )}
+
+          <li>
+            <label className="grid cursor-pointer place-items-center">
+              <input
+                type="checkbox"
+                value="dark"
+                className="col-span-2 col-start-1 row-start-1 toggle theme-controller bg-base-content"
+              />
+              <svg
+                className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx="12" cy="12" r="5" />
+                <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+              </svg>
+              <svg
+                className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+              </svg>
+            </label>
+          </li>
         </ul>
       </div>
       <div className="navbar-end">
@@ -114,7 +153,7 @@ const Navbar = () => {
               {user ? (
                 <div>
                   <details className="z-50 dropdown dropdown-end">
-                    <summary className="bg-white border-none rounded-full shadow-none outline-none hover:bg-white btn">
+                    <summary className="bg-white border-none rounded-full shadow-none outline-none dark:bg-base-100 hover:bg-base-100 btn">
                       <img
                         src={user?.photoURL}
                         alt="user"
