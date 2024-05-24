4e4b9f635b81c4988c4fb460db66535837bceca1
jakareya76 <jakareya1306@gmail.com>
2024-05-24 11:09:54 +0600

    add queries in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../components/About";
 import Banner from "../components/Banner";
 import FAQ from "../components/FAQ";
+import Queries from "./Queries";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Queries />
       <About />
       <FAQ />
     </div>
--- src/pages/Queries.jsx
+++ src/pages/Queries.jsx
@@ -1,38 +1,3 @@
-// import { useEffect, useState } from "react";
-// import QuerieCard from "../components/QuerieCard";
-// import api from "../api/api";
-
-// const Queries = () => {
-//   const [queries, setQueries] = useState([]);
-
-//   useEffect(() => {
-//     const getQueriesData = async () => {
-//       const res = await api.get("/queries");
-
-//       setQueries(res.data);
-//     };
-
-//     getQueriesData();
-//   }, []);
-
-//   return (
-//     <div className="container mx-auto">
-//       <div className="my-8">
-//         <h2 className="text-3xl font-bold text-center">All Queries</h2>
-//         <div className="flex items-center justify-center mt-8">
-//           <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
-//             {queries.map((item) => {
-//               return <QuerieCard key={item._id} querie={item} />;
-//             })}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Queries;
-
 import { useEffect, useState } from "react";
 import QuerieCard from "../components/QuerieCard";
 import api from "../api/api";
